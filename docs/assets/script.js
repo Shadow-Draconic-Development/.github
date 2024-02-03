5556b437bdc135e90868fc83b6ec1cc51c43eafb
SethHartman13 <hartmanse@byui.edu>
2024-02-02 21:55:48 -0700

    auto-script

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -29,4 +29,17 @@ window.onload = function() {
     if (typeof PatreonButton !== 'undefined' && PatreonButton !== null) {
         PatreonButton.init();
     }
+
+    // Calculate age dynamically based on June 13, 2001
+    const birthDate = new Date('2001-06-13');
+    const currentDate = new Date();
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    // Update about me text with dynamic age
+    const aboutMe = document.getElementById('about-me');
+    aboutMe.textContent = `I am a ${age} year old college student who is currently pursuing a Bachelor's Degree in Computer Science with a minor in Engineering. I enjoy writing content libraries because of how intuitive Avrae is and how I can make the lives of users easier with quality automation. While I do this for free, taking the time to donate a few dollars allows me to dedicate more time to these projects as I have a family that I have to help take care of on top of schooling and work.`;
 };
